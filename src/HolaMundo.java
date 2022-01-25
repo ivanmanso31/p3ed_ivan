74f21bbbbf17419d53dd96766321935723ac4eeb
IvanManso <ivan.mg313@gmail.com>
2022-01-25 14:17:47 +0100

    Añadidos comentarios a HolaMundo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,8 +1,13 @@
-
+/**
+ *
+ * @author ivan
+ */
 public class HolaMundo {
-
+	 /**
+     * @param args the command line arguments
+     */
 	public static void main(String[] args) {
-		
+		//Imprime en pantalla Hola Mundo
 		System.out.println("Hola Mundo");
 
 	}
